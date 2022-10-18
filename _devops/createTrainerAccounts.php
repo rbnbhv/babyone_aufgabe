3258e7ce6564ebf70671c39d70a0d2da9134ac63
Ruben Allenstein <ru.allenstein@reply.de>
2022-10-18 20:03:54 +0200

    [UPDATE] change email of trainer (duplication)

--- _devops/createTrainerAccounts.php
+++ _devops/createTrainerAccounts.php
@@ -5,7 +5,7 @@ $trainer = [];
 
 $trainer[] = ['forename' => 'Anton', 'email' => 'anton@btv.de', 'password' => 'anton123'];
 $trainer[] = ['forename' => 'Chris', 'email' => 'chris@btv.de', 'password' => 'chris123'];
-$trainer[] = ['forename' => 'Bert', 'email' => 'anton@btv.de', 'password' => 'bert123'];
+$trainer[] = ['forename' => 'Bert', 'email' => 'bert@btv.de', 'password' => 'bert123'];
 
 foreach($trainer as $item) {
     registerUser($item['forename'], $item['email'], $item['password']);
