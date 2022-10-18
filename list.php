81ea1c7507e99729607e7150c4905f93f5d8df33
Ruben Allenstein <ru.allenstein@reply.de>
2022-10-18 17:32:28 +0200

    [UPDATE] updated member list and changed data visibility

--- list.php
+++ list.php
@@ -1,69 +1,69 @@
 <?php
-include('mysql.php');
+include('includes/functions.php');
+include('views/header.php');
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Mitgliederliste</title>
-</head>
-<body>
-
-<div class="container">
-    <div class="col-md-12 mt-4 card">
-        <div class="card-header">
-            <h3>Mitgliederliste</h3>
-        </div>
-        <div class="card-body">
-            <table class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Vorname</th>
-                    <th>Email</th>
-                    <th>Mitgliedsdetails</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-                $query = "SELECT * FROM member_v1";
-                $stmt = $mysql->prepare($query);
-                $stmt->execute();
-
-                $stmt->setFetchMode(PDO::FETCH_OBJ); //PDO::FETCH_ASSOC
-                $result = $stmt->fetchAll();
-                if ($result) {
-                    foreach ($result as $row) {
-                        ?>
-                        <tr>
-                            <td><?php echo $row->id; ?></td>
-                            <td><?php echo $row->forename ?></td>
-                            <td><?php echo $row->email ?></td>
-                            <td>
-                                <a href="view.php?id=<?php echo $row->id ?>" class="btn btn-primary">Details anzeigen</a>
-                                <a href="edit.php?id=<?php echo $row->id ?>" class="btn btn-warning">Details bearbeiten</a>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-
-                } else {
-                    ?>
+    <!-- list Section -->
+    <section id="list">
+        <div class="list container">
+            <div class="list-header">
+                <h1 class="section-title"><span>BTV </span>Mitglieder</h1>
+                <h2>Mitgliederliste</h2>
+                <table class="table">
+                    <thead>
                     <tr>
-                        <td colspan="5">Kein Eintrag gefunden</td>
+                        <th>Mitgliedsnr</th>
+                        <th>Vorname</th>
+                        <th>Email</th>
+                        <th>Optionen</th>
                     </tr>
+                    </thead>
+                    <tbody>
                     <?php
-                }
-                ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+                    session_start();
+                    if (isset($_SESSION["email"])) {
+                        $mysql = getMysqlConnection();
+                        $stmt = $mysql->prepare("SELECT * FROM member_v1");
+                        $stmt->execute();
+                        $stmt->setFetchMode(PDO::FETCH_OBJ);
+                        $result = $stmt->fetchAll();
+                        if ($result) {
+                            foreach ($result as $row) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $row->id; ?></td>
+                                    <td><?php echo $row->forename ?></td>
+                                    <td><?php echo $row->email ?></td>
+                                    <td>
+                                        <a href="view.php?id=<?php echo $row->id ?>"
+                                           class="btn">Mitglied anzeigen</a>
+                                        <a href="editUserDetails.php?id=<?php echo $row->id ?>"
+                                           class="btn">Mitglied bearbeiten</a>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+                        } else {
+                            ?>
+                            <tr>
+                                <td>Kein Eintrag gefunden</td>
+                            </tr>
+                            <?php
+                        }
+
+                    } else {
+                        header("Location: login.php");
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <!-- End list Section -->
+
+
+<?php
+include('views/footer.php')
+?>
