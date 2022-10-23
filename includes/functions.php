5360bb8b26c5127cff90ae2e355faca498895fbe
Ruben Allenstein <ru.allenstein@reply.de>
2022-10-23 16:27:13 +0200

    [UPDATE] refactoring user and session functions

--- includes/functions.php
+++ includes/functions.php
@@ -1,53 +1,20 @@
 <?php
+session_start();
 include('Court.php');
+include('User.php');
 
 const SERVERNAME = 'localhost';
 const USERNAME = 'root';
 const PASSWORD = 'root';
 const DATABASE = 'tennis_digital';
+const DATEFORMAT_DB = 'Y-m-d H:i';
+const DATEFORMAT_FRONTEND = 'd.m.Y H:i';
 
 function getMysqlConnection(): PDO
 {
     $mysql = new PDO('mysql:host=' . SERVERNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
     $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $mysql;
-}
-
-function registerUser(string $forename, string $email, string $password, bool $isTrainer, string $phonenumber): void
-{
-    $mysql = getMysqlConnection();
-    $stmt = $mysql->prepare("INSERT INTO member_v1 (forename, email, password, isTrainer, phonenumber) VALUES (:forename, :email, :password, :isTrainer, :phonenumber)");
-    $stmt->bindParam(':forename', $forename);
-    $stmt->bindParam(':email', $email);
-    $hash = password_hash($password, PASSWORD_BCRYPT);
-    $stmt->bindParam(':password', $hash);
-    $stmt->bindParam(':isTrainer', $isTrainer);
-    $stmt->bindParam(':phonenumber', $phonenumber);
-    $stmt->execute();
-}
-
-function getUserInformation(string $id): array
-{
-    $mysql = getMysqlConnection();
-    $stmt = $mysql->prepare("select * FROM member_v1 WHERE `id` = :id");
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll();
-}
-
-function editUserDetails(int $id, string $forename, string $phonenumber): bool
-{
-    $mysql = getMysqlConnection();
-    $stmt = $mysql->prepare("UPDATE member_v1 SET forename = :forename, phonenumber = :phonenumber WHERE id = :id");
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->bindParam(':forename', $forename);
-    $stmt->bindParam(':phonenumber', $phonenumber);
-    $stmt->execute();
-    if ($stmt->execute()) {
-        return true;
-    } else {
-        return false;
-    }
 }
 
 function isTrainer(string $id): bool
@@ -58,15 +25,4 @@ function isTrainer(string $id): bool
     $stmt->execute();
     $item = $stmt->fetch();
     return $item['isTrainer'];
-}
-
-function isReserved(int $court, string $date): bool
-{
-    $mysql = getMysqlConnection();
-    $stmt = $mysql->prepare("SELECT * FROM reservation WHERE court_id = :courtId AND date = :date");
-    $stmt->bindParam(':courtId', $court);
-    $stmt->bindParam(':date', $date);
-    $stmt->execute();
-    $item = $stmt->fetch();
-    return is_array($item);
 }
